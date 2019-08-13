325102c1e6f2a3a0c9ae44067e1a3642875a8728
cryptochangements34 <bW9uZXJv@gmail.com>
2019-08-13 12:12:25 -0400

    add more control over out key used in generate_genesis_tx

--- monerolib/transactions.py
+++ monerolib/transactions.py
@@ -6,10 +6,12 @@ from binascii import hexlify, unhexlify
 
 # descr: generates a version 1 coinbase tx with one output and unlock height 60
 # params:
-# view_key: public viewkey of recipient
+# view_key: secret viewkey of recipient
 # spend_key: public spendkey of recipient
+# tx_key: public transaction key
+# amount: amount in atomic units
 
-def generate_genesis_tx(view_key, spend_key, amount):
+def generate_genesis_tx(view_key, spend_key, tx_key, amount):
     version = "01"
     unlock_height = "3c"
     vin_count = "01"
@@ -18,7 +20,7 @@ def generate_genesis_tx(view_key, spend_key, amount):
     vout_count = "01"
     out_amount = varint.encodeVarInt(amount)
     out_type = "02"
-    key_data = stealth_address.generate_rand_output(spend_key, view_key, "00")
-    vout_key = key_data[0]
-    extra = "2101" + key_data[1] + "00"
+    key_derivation = crypto.generate_key_derivation(tx_key, view_key)
+    vout_key = stealth_address.derive_public_key(key_derivation, "00", spend_key)
+    extra = "2101" + tx_key + "00"
     return version + unlock_height + vin_count + coinbase_vin + gen_height + vout_count + out_amount + out_type + vout_key + extra
